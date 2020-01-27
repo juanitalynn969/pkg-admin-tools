52e473ae26cc122c2b83f8413c258f1d80b3418e
Nigel Bowden <wifinigel@gmail.com>
2020-01-27 06:18:03 +0000

    added pkg installer & rollback

--- modules/installer_utils.py
+++ modules/installer_utils.py
@@ -104,6 +104,81 @@ def check_module_installed(mod_name):
     else:
         return False
 
+def pkg_install(branch, pkg_name, linux_pkg_list, backup_dir, base_dir, module_dir, tmp_dir, github_url, install_dir) :
+
+    # check we can get to GitHub
+    if not check_internet():
+        return False
+
+    print("Installing {}...".format(pkg_name))
+
+    # check we have pre-requisite Linux modules (apt-get etc.)
+    if not check_pkgs_installed(linux_pkg_list):
+        return False
+    
+    # check we have pre-requisite python modules
+    # TBA
+
+    # remove old backup file if exists
+    print("Checking if old backup exists.")
+    if file_exists(backup_dir):
+        print("Old backup dir exists.")
+        
+        if not clear_dir(backup_dir):
+            return False
+    
+    # change in to home dir
+    print("Changing directory.")
+    if not change_directory(base_dir):
+        return False
+
+    # move existing module dir to tmp dir
+    if file_exists(module_dir):
+        print("Backing up existing module files...")
+
+        if not move_directory(module_dir, tmp_dir):
+            return False
+
+    # clone files from GitHub
+    if not pull_github_files(github_url, branch):
+        return False
+
+    # Set file permissions
+    print("Setting permissions on new files")
+    try:
+        script_output = subprocess.check_output("sh {}/set_file_permissions.sh".format(install_dir), shell=True)
+        print("File permissions set.")
+    except:
+        print("Unable to set file permissions.")
+        return False
+
+    print("Install complete.")
+    return True
+
+def pkg_rollback(pkg_name, backup_dir, install_dir, base_dir):
+
+    print("Rolling back {} installation...".format(pkg_name))
+    print("Checking if backup exists.")
+    if file_exists(backup_dir):
+        print("Backup file exists, rolling back.")
+    
+    print("Clearing install directory: {}".format(install_dir))
+
+    if clear_dir(install_dir):
+        print("Module directory cleared, restoring files...")
+    else:
+        print("Unable to clear existing module directory - failed.")
+        return False
+
+    if (move_directory(backup_dir, base_dir)):
+        print("Files restored to {} OK.".format(base_dir))
+    else:
+        print("Unable to restore files to {}.".format(base_dir))
+        return False 
+
+    print("Rollback complete.")
+    return True
+
 
 
 
