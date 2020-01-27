54d4994e5032bff531e23c8ce36d6b8972fbcd91
Nigel Bowden <wifinigel@gmail.com>
2020-01-27 06:18:49 +0000

    moved profiler to std pkg installer

--- modules/profiler.py
+++ modules/profiler.py
@@ -12,89 +12,21 @@ module_dir = 'profiler'
 install_dir = "{}/{}".format(base_dir, module_dir)
 tmp_dir = '/tmp'
 backup_dir = "{}/profiler".format(tmp_dir)
-github_repo = "github.com/WLAN-Pi/profiler.git"
+github_url = "github.com/WLAN-Pi/profiler.git"
+pkg_name = 'profiler'
+linux_pkg_list = []
 
 def profiler_install(branch) :
 
-    # check we can get to GitHub
-    if not check_internet():
-        return False
-
-    print("Installing profiler...")
-
-    # check we have pre-requisite Linux modules (apt-get etc.)
-    # TBA
-
-    # check we have pre-requisite python modules
-    # fakeAP, manuf
-    #if check_module_installed('manuf'):
-    #    print("Yes...")
-    
-    #exit()
-
-        # remove old backup file if exists
-    print("Checking if old backup exists.")
-    if file_exists(backup_dir):
-        print("Old backup dir exists.")
-        
-        if not clear_dir(backup_dir):
-            return False
-    
-    # change in to home dir
-    print("Changing directory.")
-    if not change_directory(base_dir):
-        return False
-
-    # move existing module dir to tmp dir
-    if file_exists(module_dir):
-        print("Backing up existing module files...")
-
-        if not move_directory(module_dir, tmp_dir):
-            return False
-
-    # clone files from GitHub
-    if not pull_github_files(github_repo, branch):
-        return False
-
-    # Set file permissions
-    print("Setting permissions on new files")
-    try:
-        script_output = subprocess.check_output("sh {}/set_file_permissions.sh".format(install_dir), shell=True)
-        print("File permissions set.")
-    except:
-        print("Unable to set file permissions.")
-        return False
-
-    print("Install complete.")
-    return True
+    return pkg_install(branch, pkg_name, linux_pkg_list, backup_dir, base_dir, module_dir, tmp_dir, github_url, install_dir)
 
 ##################################################################
 # Rollback profiler installation:
 #
 # 1. Check if /tmp/profiler dir exists
-# 2. clear hotspot dir (/home/wlanpi/profiler)
+# 2. clear profiler dir (/home/wlanpi/profiler)
 # 3. Copy files in /tmp/profiler back to /home/wlanpi/profiler 
 ##################################################################
 def profiler_rollback():
 
-    print("Rolling back profiler installation...")
-    print("Checking if backup exists.")
-    if file_exists(backup_dir):
-        print("Backup file exists, rolling back.")
-    
-    print("Clearing install directory: {}".format(install_dir))
-
-    if clear_dir(install_dir):
-        print("Module directory cleared, restoring files...")
-    else:
-        print("Unable to clear existing module directory - failed.")
-        return False
-
-    if (move_directory(backup_dir, base_dir)):
-        print("Files restored to {} OK.".format(base_dir))
-    else:
-        print("Unable to restore files to {}.".format(base_dir))
-        return False 
-
-    print("Rollback complete.")
-    return True
+    return pkg_rollback(pkg_name, backup_dir, install_dir, base_dir)
